65ecaebde2cc6602df2f2f1829d7514753fcfd9d
Prathmesh Pandya <prathmesh.pandya@krishtechnolabs.com>
2025-04-02 16:00:46 +0530

    configure crs

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+    origin: ['https://krish-chatbot-ui.vercel.app', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 86400 // 24 hours
+};
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
